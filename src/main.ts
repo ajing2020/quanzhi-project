b384919bf73266c6783d845d376449712ea67b8a
ajing2020 <1277483876@qq.com>
2022-03-22 11:17:30 +0800

    配置代理解决跨域问题

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+axios.defaults.baseURL = 'http://localhost:8080/api/'
+axios.interceptors.request.use(config => {
+  config.params = { ...config.params }
+  return config
+})
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
 app.mount('#app')
+axios.get('/columns').then(res => {
+  console.log(res)
+})
